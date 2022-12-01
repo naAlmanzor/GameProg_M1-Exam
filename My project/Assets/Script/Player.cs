ff73f2b99b2f3830f49b41e034db9782b4ba2a1b
naAlmanzor <neilalmanzor28@gmail.com>
2022-12-01 17:14:10 +0800

    Added player script

---
+++ My project/Assets/Script/Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{   
+    bool onJumpInput;
+    float onHorizontalInput;
+    float onVerticalInput;
+
+
+    Rigidbody rb;
+    [SerializeField]private GameObject onGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            onJumpInput = true;
+        }
+
+        onHorizontalInput = Input.GetAxis("Horizontal");
+        onVerticalInput = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(onHorizontalInput, rb.velocity.y, onVerticalInput );
+
+        if (Physics.OverlapSphere(onGround.transform.position, 0.01f).Length == 0)        
+        {
+            return;
+        }
+
+        if(onJumpInput==true){
+            rb.AddForce(Vector3.up*3f,ForceMode.VelocityChange);
+            onJumpInput = false;
+        }
+    }
+
+    public void CreateClassMethod()
+    {
+        Debug.Log("You referenced a class!");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "coin"){
+            Debug.Log("Interacting with Coin.");
+        }
+    }
+}
